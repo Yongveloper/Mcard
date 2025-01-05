f9d3ea0f504fbd02d4c7ffafaa8cada5eb57e89a
yongveloper <dev_yong@kakao.com>
2025-01-05 18:26:30 +0900

    feat: 카드 신청 구현 (2) - 약관동의

---
+++ src/constants/apply.ts
@@ -0,0 +1,13 @@
+import { ITerm } from '@/models/apply';
+
+export const 약관목록: ITerm[] = [
+  {
+    id: '01',
+    title: '카드신청 관련 안내 및 필수 동의',
+  },
+  {
+    id: '02',
+    title: '(필수) 개인 정보 요약 동의서',
+    link: 'https://google.com',
+  },
+];
